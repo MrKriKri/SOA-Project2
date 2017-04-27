504c1d3eb6dd52fb32691ec34a64612f4d9d671f
MrKriKri <stura.wasit@gmail.com>
2017-04-28 02:30:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,4 @@
-<?php header( 'Location: /bot.php' ) ;  ?>
+http://977377searchproducts.azurewebsites.net/api/v1/Search/Products/ProductName/{}  ProductName ====> ProductID
+http://977-377demoapi.azurewebsites.net/OrderDetail/Search/ProductID/{}  ProductID ====> saleOrderID{OrderQty,UnitPrice}
+http://977377saleorderheader.azurewebsites.net/api/v1/salesOrderHeader/SalesOrderID/{} saleOrderID ===> CustomerID
+http://customerservices.azurewebsites.net/Help/Api/Customers/Search/CustomerID/{} CustomerID ===> CustomerName
